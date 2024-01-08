bb522ce8e81e3330a3a6c7b877ba561c6d5d00cd
88potatoes <94770511+88potatoes@users.noreply.github.com>
2024-01-08 17:59:45 +1100

    initial

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+import Card from './Card'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>WhoDaFaker</div>
+      <div>
+        <Card/>
+      </div>
+    </>
+  )
+}
+
+export default App
