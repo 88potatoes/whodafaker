f2f616355e4877ada2ecfc7943adfe7bfea39648
88potatoes <94770511+88potatoes@users.noreply.github.com>
2024-01-10 19:34:13 +1100

    home page has links

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,18 @@
+import { Link } from "react-router-dom";
+
 function Home() {
     return ( <div className="container text-center">
         <h1>Who's the Faker?</h1>
+        <div className="m-2">
+            <Link to="/login">
+                <button>Sign in</button>
+            </Link>
+        </div>
+        <div className="m-2">
+            <Link to="/signup">
+                <button>Sign up</button>
+            </Link>
+        </div>
     </div> );
 }
 
