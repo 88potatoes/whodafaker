0bd143bd43f4b307897d29ffb0c30e2712ff7377
88potatoes <94770511+88potatoes@users.noreply.github.com>
2024-01-12 00:16:26 +1100

    joining as host works

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,10 +1,25 @@
 import { useParams } from "react-router-dom";
 import CardGrid from "../CardGrid";
+import { useEffect, useState } from "react";
+import { XSocketClient } from "phonesocket/xclient";
+import { ws_send } from "phonesocket";
 
 function Room() {
     const params = useParams();
+    const players = useState<number[]>([])
     const roomCode = params.roomCode;
     console.log(roomCode);
+
+    useEffect(() => {
+        const xsc = new XSocketClient('phone', "ws://localhost:9090");
+
+        xsc.onopen = () => {
+            ws_send(xsc, "join_room_host", {roomCode: roomCode})
+            console.log("opened")
+        }
+        
+        // xsc.register_event("create_room", {});
+    });
 
     return ( 
     <div>
