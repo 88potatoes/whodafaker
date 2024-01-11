86cc71473b126f2403925cdc435eba8a4c370fa7
88potatoes <94770511+88potatoes@users.noreply.github.com>
2024-01-12 01:53:56 +1100

    join works (but is double join)

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,12 +1,11 @@
-import { useParams } from "react-router-dom";
-import CardGrid from "../CardGrid";
-import { useEffect, useState } from "react";
-import { XSocketClient } from "phonesocket/xclient";
 import { ws_send } from "phonesocket";
+import { XSocketClient } from "phonesocket/xclient";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 function Room() {
     const params = useParams();
-    const players = useState<number[]>([])
+    const [players, setPlayers] = useState<number[]>([])
     const roomCode = params.roomCode;
     console.log(roomCode);
 
@@ -17,9 +16,19 @@ function Room() {
             ws_send(xsc, "join_room_host", {roomCode: roomCode})
             console.log("opened")
         }
+
+        xsc.register_event('player_join', (data) => {
+            // console.log("player_join:", data);
+            // console.log(players);
+            // data should be {playerId: playerId}
+            setPlayers(data.players)
+            // console.log(players)
+            // console.log(typeof players); // Should log "object"
+            // console.log(Array.isArray(players)); // should log "true"
+        })
         
         // xsc.register_event("create_room", {});
-    });
+    }, []);
 
     return ( 
     <div>
@@ -28,6 +37,9 @@ function Room() {
         </div>
         <div className="row">
             <h2>Players</h2>
+            {players.map((number, index) => {
+                return <div className="col-3" key={index}><h1>{number}</h1></div>
+            })}
         </div>
         <div>
             <h2>Set</h2>
