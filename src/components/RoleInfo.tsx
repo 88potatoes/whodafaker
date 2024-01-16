c480c69b40b7cd864fa044489a08b5d7e652671c
88potatoes <94770511+88potatoes@users.noreply.github.com>
2024-01-16 16:05:44 +1100

    roleinfo component

---
+++ src/components/RoleInfo.tsx
@@ -0,0 +1,22 @@
+function RoleInfo({ img, goal, info, genstrat, title }) {
+    return ( 
+    <div className="my-2 secondarysection">
+        <div className="px-2 pt-1">
+            <h5 className="pb-0 mb-0">{title}</h5>
+        </div>
+        <div className="d-flex">
+            <div className="d-flex justify-content-end align-items-center m-2">
+                <img src={img} alt="villager" width={150} height={150} className="mb-3 rounded roleimage"/>
+            </div>
+            <div className="d-flex align-items-center">
+                <div className="">
+                    <p><strong>Goal:</strong> {goal} </p>
+                    <p><strong>Info:</strong> {info} </p>
+                    <p><strong>General Strategy: </strong>{genstrat}</p>
+                </div>
+            </div>
+        </div>
+    </div> );
+}
+
+export default RoleInfo;
