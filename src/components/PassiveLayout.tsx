d70f651363df5c26ee2be83e1543cc54043cfe1d
88potatoes <94770511+88potatoes@users.noreply.github.com>
2024-01-27 17:14:54 +1100

    documenting passivelayout and screenwindow

--- src/components/PassiveLayout.tsx
+++ src/components/PassiveLayout.tsx
@@ -4,6 +4,11 @@ interface OnlyChildProps {
     children: ReactNode
 }
 
+/**
+ * Gives an element with centered flex column and width, height of 100%. Used for as the container for all pages.
+ * @param children
+ * @returns 
+ */
 function PassiveLayout({ children }: OnlyChildProps) {
     return (<div className="w-100 h-100 d-flex flex-column justify-content-center">
         {children}
--- src/components/ScreenWindow.tsx
+++ src/components/ScreenWindow.tsx
@@ -1,5 +1,10 @@
 import { OnlyChildProps } from "./PassiveLayout";
 
+/**
+ * JSX Element wrapper which gives white box with height of 75% the screen and a flex column
+ * @param children 
+ * @returns 
+ */
 function ScreenWindow({ children }: OnlyChildProps) {
     return (
         <div className="container hscreen d-flex flex-column justify-content-center">
