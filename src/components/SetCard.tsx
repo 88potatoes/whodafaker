878f1b6398d16dbfd2ad1a005eed39b4d9da5d52
88potatoes <94770511+88potatoes@users.noreply.github.com>
2024-01-28 15:23:19 +1100

    moved SetCard to components

--- src/components/SetCard.tsx
+++ src/components/SetCard.tsx
@@ -1,3 +1,4 @@
+import { motion } from "framer-motion";
 import { useNavigate } from "react-router-dom";
 
 interface setProps {
@@ -9,14 +10,17 @@ function SetCard(props: setProps) {
     const linkTo = `/createset/${props.setId}`;
     const navigate = useNavigate();
     return ( 
-        <div
-        className="col-3 d-flex flex-column justify-content-center align-items-center">
+        <motion.div
+        className="col-6 col-md-4 col-lg-3 d-flex flex-column justify-content-center align-items-center"
+        whileHover={{ rotate: `${Math.random()*6-3}deg`, scale: 1.1 }}
+        whileTap={{ rotate: `${Math.random()*6-3}deg`, scale: 0.9 }}
+        >
             <div onClick={() => {
             navigate(linkTo)
             }} className="col-11 bg-primary rounded hoverablecard text-center my-2 py-5">
                 <h2 className="text-light">{props.word}</h2>
             </div>
-        </div>    
+        </motion.div>    
      );
 }
 
