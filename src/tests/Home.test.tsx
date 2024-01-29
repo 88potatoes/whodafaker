42690fc142b877ef497064f7114c8eee7498cfea
88potatoes <94770511+88potatoes@users.noreply.github.com>
2024-01-29 12:15:44 +1100

    home page and navigation tests

--- src/tests/Home.test.tsx
+++ src/tests/Home.test.tsx
@@ -1,8 +1,13 @@
 import { describe, it, expect } from "vitest";
 import * as matchers from "@testing-library/jest-dom/matchers"
-import { render, screen } from "@testing-library/react"
-import { MemoryRouter } from "react-router-dom";
+import { fireEvent, render, screen } from "@testing-library/react"
+import { MemoryRouter, Route, Routes } from "react-router-dom";
 import Home from "../pages/Home";
+import App from "../App";
+import { createRoot } from "react-dom/client";
+import Login from "../pages/Login";
+import JoinRoom from "../pages/JoinRoom";
+import Signup from "../pages/Signup";
 
 expect.extend(matchers);
 
@@ -10,43 +15,108 @@ expect.extend(matchers);
 describe("Home screen", () => {
     it("should have title", () => {
         render(
-        <MemoryRouter>
-            <Home/>
-        </MemoryRouter>);
-        expect(screen.getAllByRole("heading")[0]).toHaveTextContent(/who's the faker?/i)
+            <MemoryRouter>
+                <Home />
+            </MemoryRouter>);
+        expect(screen.getAllByRole("heading")[0]).toHaveTextContent(/who's the faker\?/i)
+
     })
 
-    it("should have join game button", () => {
-        render(
-        <MemoryRouter>
-            <Home/>
+    it("should have join game button and the button should redirect to '/join'", () => {
+
+        const { getAllByText } = render(<MemoryRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/join" element={<JoinRoom />} />
+            </Routes>
         </MemoryRouter>);
-        expect(screen.getByText(/join game/i)).toBeInTheDocument();
+
+        // Starts at '/'
+
+        // Go to '/join'
+        const joinButton = getAllByText(/join room/i)[0];
+        fireEvent.click(joinButton);
+        expect(getAllByText(/join room/i)[0]).toBeInTheDocument();
+
     })
 
-    it("should have a sign in button", () => {
-        render(
-        <MemoryRouter>
-            <Home/>
+    it("should have a sign in button which takes you to the sign in page '/login'", () => {
+
+        const { getAllByText } = render(<MemoryRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/login" element={<Login />} />
+            </Routes>
         </MemoryRouter>);
-        expect(screen.queryByText(/sign in/i)).not.toBe(null);
-    })
-    
-    it("should have a sign up button", () => {
-        render(
-        <MemoryRouter>
-            <Home/>
-        </MemoryRouter>);
-        expect(screen.queryByRole("button", { name: /sign up/i})).not.toBe(null);
+
+
+        const signInButton = getAllByText(/Log in/i)[0];
+        fireEvent.click(signInButton);  // go to '/login'
+        expect(getAllByText(/Login/i)[0]).toBeInTheDocument();
     })
 
-    it("should display the rules", () => {
-        render(
-        <MemoryRouter>
-            <Home/>
+    it("should have a sign up button which takes you to the sign up page '/signup'", () => {
+
+        const { getAllByText } = render(<MemoryRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/signup" element={<Signup />} />
+            </Routes>
         </MemoryRouter>);
-        expect(screen.queryByText(/who's the faker\? rules/i)).toBeInTheDocument();
+
+
+        const signUpButton = getAllByText(/Sign up/i)[0];
+        fireEvent.click(signUpButton);  // go to '/signup'
+        expect(getAllByText(/Sign up/i)[0]).toBeInTheDocument();
     })
+
+    it("should be able to smoothly navigate between unauthorised pages", () => {
+        const { getAllByText } = render(<MemoryRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/signup" element={<Signup />} />
+                <Route path="/join" element={<JoinRoom />} />
+            </Routes>
+        </MemoryRouter>);
+
+        // / -> /join -> / -> /login -> /signup -> / -> /signup -> /login
+
+        // / -> /join
+        fireEvent.click(getAllByText(/join room/i)[0]);
+        expect(getAllByText(/join room/i)[0]).toBeInTheDocument();
+        
+        // /join -> /
+        fireEvent.click(getAllByText(/Home/i)[0]);
+        expect(getAllByText(/Who's the Faker\?/i)[0]).toBeInTheDocument();
+        
+        // / -> /login
+        fireEvent.click(getAllByText(/Log in/i)[0]);
+        expect(getAllByText(/Login/i)[0]).toBeInTheDocument();
+
+        // /login -> /signup
+        fireEvent.click(getAllByText(/Sign up/i)[0]);
+        expect(getAllByText(/Sign up/i)[0]).toBeInTheDocument();
+
+        // /signup -> /
+        fireEvent.click(getAllByText(/home/i)[0]);
+        expect(getAllByText(/who's the faker\?/i)[0]).toBeInTheDocument();
+
+        // / -> /signup
+        fireEvent.click(getAllByText(/sign up/i)[0]);
+        expect(getAllByText(/sign up/i)[0]).toBeInTheDocument();
+
+        // /signup -> /login
+        fireEvent.click(getAllByText(/Log in/i)[0]);
+        expect(getAllByText(/Login/i)[0]).toBeInTheDocument();
+    })
+    // it("should display the rules", () => {
+    //     render(
+    //         <MemoryRouter>
+    //             <Home />
+    //         </MemoryRouter>);
+    //     expect(screen.queryByText(/who's the faker\? rules/i)).toBeInTheDocument();
+    // })
 
     // buttons should redirect to correct locations
 })
